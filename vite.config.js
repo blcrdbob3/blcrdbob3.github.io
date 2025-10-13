b03cbd0b769925f93179791ea3efb95c5fd3ece3
blcrdbob3 <25latthe@gmail.com>
2025-10-13 13:08:41 -0400

    Testing MPA (w/o 404 hack)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        projects: path.resolve(__dirname, 'projects.html'),
+        mechatronics: path.resolve(__dirname, 'projects/mechatronics.html'),
+        introAI: path.resolve(__dirname, 'projects/intro-to-ai.html')
+      }
+    }
+  }
+});
