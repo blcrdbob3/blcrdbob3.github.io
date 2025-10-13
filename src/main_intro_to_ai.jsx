df3b82634fc6e106810a3106fbca653921abb4b5
blcrdbob3 <25latthe@gmail.com>
2025-10-13 14:05:30 -0400

    Fixed typo for imports

--- src/main_intro_to_ai.jsx
+++ src/main_intro_to_ai.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ProjectIntroAI from "./pages/ProjectIntroAI.jsx";
+import ProjectIntroAI from "./pages/IntroToAI.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/main_mechatronics.jsx
+++ src/main_mechatronics.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ProjectMechatronics from "./pages/ProjectMechatronics.jsx";
+import ProjectMechatronics from "./pages/Mechatronics.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
